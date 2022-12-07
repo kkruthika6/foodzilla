6195d30170a3d252fc7f53166a72ef854eb7788a
kkruthika6 <kruthikak@Kruthikas-MacBook-Pro.local>
2022-12-07 15:46:34 -0500

    Connected to sidebar

--- foodzilla-app/src/pages/Dashboard/Dashboard.js
+++ foodzilla-app/src/pages/Dashboard/Dashboard.js
@@ -5,11 +5,12 @@ import { Container, Col, Alert, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import './Dashboard.css';
+import SideBar from '../../components/SideBar';
 
 function Dashboard() {
     const [feed, setFeed] = useState([]);
     const [click, setClick] = useState(false);
-    
+
     useEffect(() => {
         if (click === true) {
             const listdata = document.getElementById("ft").value;
@@ -17,57 +18,47 @@ function Dashboard() {
             const location = "http://maps.google.com/maps?q=" + encodeURIComponent(address);
             const contact = document.getElementById("ph").value;
             const list = feed;
-            list.push({info: listdata, loc: location, address: address, contact: contact});
+            list.push({ info: listdata, loc: location, address: address, contact: contact });
             setFeed(list);
             setClick(false);
-            document.getElementById("ft").value='';
-            document.getElementById("location").value='';
-            document.getElementById("ph").value='';
+            document.getElementById("ft").value = '';
+            document.getElementById("location").value = '';
+            document.getElementById("ph").value = '';
         }
     }, [click])
 
     return (
         <>
+            <SideBar />
+            <br />
+            <h2>Have Leftovers?</h2>
             <div class="container">
                 <Container>
-                    <Col md="3">
-                        <h3></h3>
-                        <Card className='title'>
-                            <Card.Body>
-                                <Card.Text>
-                                    <br /><br /><br /><br /><br /><br />
-                                    <h2>Have Leftovers?</h2>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md="11">
-                        <h3></h3>
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Check the feed!</Card.Title>
-                                <Card.Text>
-                                    <div className='feed'>
-                                        {feed !== null ? feed.map((f) => {
-                                            return(
+                <br/>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Check the feed!</Card.Title>
+                            <Card.Text>
+                                <div className='feed'>
+                                    {feed !== null ? feed.map((f) => {
+                                        return (
                                             <Alert className='feedtext'>
                                                 {f.info + ' @ '}
-                                                <a href={f.loc} target="_blank">{f.address}</a><br/>{" Please contact on "}
+                                                <a href={f.loc} target="_blank">{f.address}</a><br />{" Please contact on "}
                                                 <a href={"https://wa.me/" + f.contact}>{"Whatsapp"}</a>
                                             </Alert>
-                                            )
-                                        }) : null}
-                                    </div>
-                                    <div className='send'>
-                                        <input type="text" placeholder='Enter message' id='ft' />
-                                        <input type="text" placeholder='Enter location' id='location' />
-                                        <input type="text" placeholder='Enter Contact number' id='ph' />
-                                        <Button variant="secondary" className='sendbtn' onClick={() => {setClick(true)}}>Send</Button>
-                                    </div>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                                        )
+                                    }) : null}
+                                </div>
+                                <div className='send'>
+                                    <input type="text" placeholder='Enter message' id='ft' />
+                                    <input type="text" placeholder='Enter location' id='location' />
+                                    <input type="text" placeholder='Enter Contact number' id='ph' />
+                                    <Button variant="secondary" className='sendbtn' onClick={() => { setClick(true) }}>Send</Button>
+                                </div>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                 </Container>
             </div>
         </>
--- foodzilla-app/src/pages/Recipes/Recipes.js
+++ foodzilla-app/src/pages/Recipes/Recipes.js
@@ -3,8 +3,8 @@ import Card from 'react-bootstrap/Card';
 import { Container, Col } from 'react-bootstrap';
 import './Recipes.css'
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 import Spinner from 'react-bootstrap/Spinner';
+import SideBar from '../../components/SideBar';
 
 
 function Recipes() {
@@ -33,93 +33,71 @@ function Recipes() {
 
     return (
         <>
-            <div className='container'>
-                <Container>
-                    <Col md="3">
-                        <h3></h3>
-                        <Card className='title'>
-                            <Card.Body>
-                                <Card.Text>
-                                    <br /><br /><br /><br /><br /><br />
-                                    <h2>Tasty Recipes</h2>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md="11">
-                        <h3></h3>
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Check the recipes!</Card.Title>
-                                <Card.Text>
-                                    <div className='feed'>
-                                        {flag === false ? <div className='loader'><Spinner animation="border" variant="dark" /></div> :
-                                            data !== undefined && data.length !== 0 ?
-                                                data.map((d) => {
-                                                    return (
-                                                        <>
-                                                            <Col md='12'>
-                                                                <Card className="card">
-                                                                    <Card.Header>{d.name}<br /></Card.Header>
-                                                                    <Card.Body>
-                                                                        <Card.Text>
-                                                                            <>
-                                                                                {d.recipes ?
-                                                                                    d.recipes.map(r => 
-                                                                                        {return(
-                                                                                            <>
-                                                                                            {r.name}<br />
-                                                                                            {r.description}
-                                                                                            <br />
-                                                                                            <img src={r.thumbnail_url} alt={r.thumbnail_alt_text} width="500px" height="300px" />
-                                                                                            <br />
-                                                                                            {r.prep_time_minutes ? "Preparation time:" + r.prep_time_minutes : null}
-                                                                                            <br />
-                                                                                            {r.yields}
-                                                                                            <br /><br />
-                                                                                            {r.instructions ? "Instructions:" : null}
-                                                                                            <ol>
-                                                                                                {r.instructions?.map((i) => { return (<li style={{ color: "black", textAlign: "left" }}>{i.display_text}<br /></li>) })}
-                                                                                            </ol>
-                                                                                            {r.user_ratings ? r.user_ratings.score !== null ? "User Rating: " + (r.user_ratings.score * 100) + '%' : null : null}
-                                                                                            <br/>
-                                                                                            </>
-                                                                                            )}
-                                                                                    ) : (<>
-                                                                                        {d.description}
-                                                                                        <br />
-                                                                                        <img src={d.thumbnail_url} alt={d.thumbnail_alt_text} width="500px" height="300px" />
-                                                                                        <br />
-                                                                                        {d.prep_time_minutes ? "Preparation time:" + d.prep_time_minutes : null}
-                                                                                        <br />
-                                                                                        {d.yields}
-                                                                                        <br /><br />
-                                                                                        {d.instructions ? "Instructions:" : null}
-                                                                                        <ol>
-                                                                                            {d.instructions?.map((i) => { return (<li style={{ color: "black", textAlign: "left" }}>{i.display_text}<br /></li>) })}
-                                                                                            <br />
-                                                                                        </ol>
-                                                                                        <br/>
-                                                                                        {d.user_ratings ? d.user_ratings.score !== null ? "User Rating: " + (d.user_ratings.score * 100) + '%' : null : null}
-                                                                                        <br/>
-                                                                                    </>)
-                                                                                }</>
-                                                                        </Card.Text>
-                                                                    </Card.Body>
-                                                                </Card>
-                                                            </Col>
+            <SideBar />
+            <br />
+            <h2>Tasty Recipes</h2>
+            <div className='recipefeed'>
+                {flag === false ? <div className='loader'><Spinner animation="border" variant="dark" /></div> :
+                    data !== undefined && data.length !== 0 ?
+                        data.map((d) => {
+                            return (
+                                <>
+                                    <Card className="recipes">
+                                        <Card.Header>{d.name}<br /></Card.Header>
+                                        <Card.Body>
+                                            <Card.Text>
+                                                <>
+                                                    {d.recipes ?
+                                                        d.recipes.map(r => {
+                                                            return (
+                                                                <>
+                                                                    {r.name}<br />
+                                                                    {r.description}
+                                                                    <br />
+                                                                    <img src={r.thumbnail_url} alt={r.thumbnail_alt_text} width="500px" height="300px" className='foodimg'/>
+                                                                    <br />
+                                                                    {r.prep_time_minutes ? "Preparation time:" + r.prep_time_minutes : null}
+                                                                    <br />
+                                                                    {r.yields}
+                                                                    <br /><br />
+                                                                    {r.instructions ? "Instructions:" : null}
+                                                                    <ol>
+                                                                        {r.instructions?.map((i) => { return (<li style={{ color: "black", textAlign: "left" }}>{i.display_text}<br /></li>) })}
+                                                                    </ol>
+                                                                    {r.user_ratings ? r.user_ratings.score !== null ? "User Rating: " + (r.user_ratings.score * 100) + '%' : null : null}
+                                                                    <br />
+                                                                </>
+                                                            )
+                                                        }
+                                                        ) : (<>
+                                                            {d.description}
                                                             <br />
-                                                        </>
-                                                    )
-                                                })
-                                                : null}
-                                    </div>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Container>
+                                                            <img src={d.thumbnail_url} alt={d.thumbnail_alt_text} width="500px" height="300px" className='foodimg'/>
+                                                            <br />
+                                                            {d.prep_time_minutes ? "Preparation time:" + d.prep_time_minutes : null}
+                                                            <br />
+                                                            {d.yields}
+                                                            <br /><br />
+                                                            {d.instructions ? "Instructions:" : null}
+                                                            <ol>
+                                                                {d.instructions?.map((i) => { return (<li style={{ color: "black", textAlign: "left" }}>{i.display_text}<br /></li>) })}
+                                                                <br />
+                                                            </ol>
+                                                            <br />
+                                                            {d.user_ratings ? d.user_ratings.score !== null ? "User Rating: " + (d.user_ratings.score * 100) + '%' : null : null}
+                                                            <br />
+                                                        </>)
+                                                    }</>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                    <br />
+                                </>
+                            )
+                        })
+                        : null}
             </div>
+
         </>
     );
 }
