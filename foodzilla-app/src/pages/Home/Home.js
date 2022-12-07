1bfb9aae9a5df1f12c17cbb77b09fd87c956486d
Kshetra Hegde <kshetrahegde9@gmail.com>
2022-12-06 23:48:25 -0500

    Sidebar added

--- foodzilla-app/src/pages/Home/Home.js
+++ foodzilla-app/src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import SideBar from '../components/SideBar'
 
 function Home() {
     return ( 
         <>
         <h1>HOME</h1> 
         <Link to='/userlogin'>User Login</Link>
-        
+        <Link to='/ownerlogin'>Owner Login</Link>
+        <SideBar/>
         
         </>
     
